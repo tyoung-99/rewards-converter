a0076fe680feb31aeeca925afd6e6e1b8bc58268
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-04-11 14:29:13 -0400

    Wrote/confirmed unit tests for RewardValue.java.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
+    private static final double CONVERSION_RATE = 0.0035;  // Miles to cash
 
     @Test
     void create_with_cash_value() {
@@ -20,11 +21,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals((int) (cashValue / CONVERSION_RATE), rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue * CONVERSION_RATE, rewardValue.getCashValue());
     }
 }
